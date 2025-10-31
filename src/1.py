d31e420c755d190b47957440ce7f72fb6d78fc3d
DMuskatin <muskatin@gmail.com>
2025-10-31 15:44:43 +0800

    исправлены ошибки в модулях masks and widgets программа заработала )

--- src/1.py
+++
@@ -1,2 +0,0 @@
-user_name = input('Введите свое имя')
-print(f'Привет {user_name}')
--- src/widget.py
+++ src/widget.py
@@ -4,7 +4,6 @@ from masks import mask_card, mask_account
 def mask_account_card(acc_number: str) -> str:
     """Возвращет исходную строку с замаскированным номером карты/счета"""
 
-
     pay_system = ("Visa", "Maestro", "MasterCard")
     for i in acc_number.split():
         if i in pay_system:
@@ -15,14 +14,11 @@ def mask_account_card(acc_number: str) -> str:
             account = "".join([i for i in acc_number if i.isdigit()])
             return f"Счет {mask_account(account)}"
 
-
 def get_date(date_and_time: str) -> str:
     """строку с датой в виде дд.мм.гггг"""
 
-
     date = [i for i in date_and_time.split("T")]
     return ".".join(list(reversed([i for i in date[0].split("-")])))
-
 
 if __name__ == '__main__':
     print(mask_account_card("Visa Platinum 7000 7922 8960 6361"))
